ccbe22d5ad3fec1e204d2703f5f5953d41292691
Brian Chirls <brian@chirls.com>
2013-05-14 13:22:43 -0400

    Add video loading into its own function

--- js/main.js
+++ js/main.js
@@ -5,34 +5,38 @@
         sourceElem = document.createElement('source'),
         vjs;
 
+    function loadVideo() {
+        vjs = VideoJS('video');
+        vjs.ready(function () {
+            var req = new XMLHttpRequest(),
+                onReqLoad,
+                chapterObject;
+
+            onReqLoad = function (response) {
+                chapterObject = JSON.parse(response.target.responseText);
+                hapyak.viewer({
+                    gzip: true,
+                    player: vjs,
+                    environment: 'feature',
+                    userId: 'kaiiscranky',
+                    apiKey: '1a88de4bb4d7f969c1282ff5910602f9',
+                    videoType: 'videojs',
+                    videoHeight: 540,
+                    videoWidth: 960,
+                    trackId: chapterObject[videoName],
+                    autoplay: false
+                });
+            };
+
+            req.onload = onReqLoad;
+            req.open('get', 'js/data.json', true);
+            req.send();
+        });
+    }
+
     sourceElem.src = 'http://video.chirls.com/geeta/' + videoName + '.mp4';
     sourceElem.type = 'video/mp4';
     document.getElementById('video').appendChild(sourceElem);
 
-    vjs = VideoJS('video');
-    vjs.ready(function () {
-        var req = new XMLHttpRequest(),
-            onReqLoad,
-            chapterObject;
-
-        onReqLoad = function(response) {
-            chapterObject = JSON.parse(response.target.responseText);
-            hapyak.viewer({
-                gzip: true,
-                player: vjs,
-                environment: 'feature',
-                userId: 'kaiiscranky',
-                apiKey: '1a88de4bb4d7f969c1282ff5910602f9',
-                videoType: 'videojs',
-                videoHeight: 540,
-                videoWidth: 960,
-                trackId: chapterObject[ videoName ],
-                autoplay: false
-            });
-        };
-
-        req.onload = onReqLoad;
-        req.open('get', 'js/data.json', true);
-        req.send();
-    });
+    loadVideo();
 })();
