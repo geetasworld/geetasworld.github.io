73ea046e8b7b7bb441affcb7560b0604262c3b66
Brian Chirls <brian@chirls.com>
2013-05-14 14:13:42 -0400

    Load tweets into variables, some text style tweeks

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
         sourceElem = document.createElement('source'),
         vjs;
 
-    function loadVideo() {
+    function loadVideo(tweets) {
         vjs = VideoJS('video');
         vjs.ready(function () {
             var req = new XMLHttpRequest(),
@@ -13,6 +13,13 @@
                 chapterObject;
 
             onReqLoad = function (response) {
+                var variables = {},
+                    i, l;
+
+                for (i = 0, l = Math.min(9, tweets.length); i < l; i++) {
+                    variables['tweet' + (i + 1)] = tweets[i].cleanedText;
+                }
+
                 chapterObject = JSON.parse(response.target.responseText);
                 hapyak.viewer({
                     gzip: true,
@@ -24,7 +31,8 @@
                     videoHeight: 540,
                     videoWidth: 960,
                     trackId: chapterObject[videoName],
-                    autoplay: false
+                    autoplay: false,
+                    variables: variables
                 });
             };
 
@@ -34,9 +42,77 @@
         });
     }
 
+    function profileTweet(tweet) {
+        var obj = {
+            entities: 0,
+            reply: /^@/.test(tweet.text) ? 1 : 0,
+            rt: /\bRT\:? @/.test(tweet.text) ? 1 : 0,
+            spam: /@womendrivers/.test(tweet.text) ? 1 : 0,
+            id: tweet.id,
+            lengthScore: 0
+        };
+
+        if (tweet.entities) {
+            obj.entities = tweet.entities.urls.length * 3 + tweet.entities.user_mentions.length * 2 + tweet.entities.hashtags.length;
+        }
+
+        tweet.cleanedText = tweet.text.replace(/(\s*@[a-zA-Z0-9_]+)+$/, '');
+        tweet.cleanedText = tweet.cleanedText.replace(/^(@[a-zA-Z0-9_]+\s*)+/, '');
+        tweet.cleanedText = tweet.cleanedText.replace(/\n{2,}/, '\n');
+
+        obj.lengthScore = Math.round(Math.abs(tweet.cleanedText.length - 80) / 5);
+
+        return obj;
+    }
+
+    function tweetSort(a, b) {
+        a = a.sortProfile;
+        b = b.sortProfile;
+
+        if (a.spam !== b.spam) {
+            return a.spam - b.spam;
+        }
+
+        if (a.reply !== b.reply) {
+            return a.reply - b.reply;
+        }
+
+        if (a.rt !== b.rt) {
+            return a.rt - b.rt;
+        }
+
+        if (a.entities !== b.entities) {
+            return a.entities - b.entities;
+        }
+
+        if (a.lengthScore !== b.lengthScore) {
+            return a.lengthScore - b.lengthScore;
+        }
+
+        return a.id - b.id;
+    }
+
+    function loadTweets() {
+        var script = document.createElement('script');
+        script.src = 'http://search.twitter.com/search.json?q=%23womendrivers&callback=receiveTweets&count=100&include_entities=true';
+        document.head.appendChild(script);
+    }
+
+    window.receiveTweets = function(data) {
+        var i,
+            tweets = data.results;
+        for (i = 0; i < tweets.length; i++) {
+            tweets[i].sortProfile = profileTweet(tweets[i]);
+        }
+
+        tweets.sort(tweetSort);
+
+        loadVideo(tweets);
+    };
+
     sourceElem.src = 'http://video.chirls.com/geeta/' + videoName + '.mp4';
     sourceElem.type = 'video/mp4';
     document.getElementById('video').appendChild(sourceElem);
 
-    loadVideo();
+    loadTweets();
 })();
