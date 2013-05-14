fcda019bde91a9af98b204d230f672943fa38321
dseif <david.c.seifried@gmail.com>
2013-05-14 11:29:23 -0400

    Fixed CSS issue causing player size to be incorect. Added a data.json file that stores the proper trackids for each track.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,38 @@
 (function () {
     'use strict';
 
-    var vjs = VideoJS('video');
+    var videoName = window.location.hash && window.location.hash.substr(1) || 'placeholder',
+        sourceElem = document.createElement('source'),
+        vjs;
+
+    sourceElem.src = 'http://video.chirls.com/geeta/' + videoName + '.mp4';
+    sourceElem.type = 'video/mp4';
+    document.getElementById('video').appendChild(sourceElem);
+
+    vjs = VideoJS('video');
     vjs.ready(function () {
-        hapyak.viewer({
-            gzip: true,
-            player: vjs,
-            environment: 'feature',
-            userId: 'kaiiscranky',
-            apiKey: '1a88de4bb4d7f969c1282ff5910602f9',
-            videoType: "videojs",
-            trackId: 2627,
-            autoplay: false
-        });
+        var req = new XMLHttpRequest(),
+            onReqLoad,
+            chapterObject;
+
+        onReqLoad = function(response) {
+            chapterObject = JSON.parse(response.target.responseText);
+            hapyak.viewer({
+                gzip: true,
+                player: vjs,
+                environment: 'feature',
+                userId: 'kaiiscranky',
+                apiKey: '1a88de4bb4d7f969c1282ff5910602f9',
+                videoType: 'videojs',
+                videoHeight: 540,
+                videoWidth: 960,
+                trackId: chapterObject[ videoName ],
+                autoplay: false
+            });
+        };
+
+        req.onload = onReqLoad;
+        req.open('get', 'js/data.json', true);
+        req.send();
     });
 })();
